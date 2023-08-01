0c304f74731675cb05067186d1a9bea9380ce664
Juan Diego A.H <j.diego93@outlook.com>
2023-07-31 22:09:12 -0600

    demo 2.0 (second end point)

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from etl import count_movies_by_original_languages, get_runtime_and_release_year
 
 app = FastAPI()
 
-@app.get("/{lang}")
+@app.get("/count_of_movies_by_language/{lang}")
 def demo_1(lang):
     return count_movies_by_original_languages(language=lang)
 
-@app.get("/{title}")
+@app.get("/get_runtime_and_release_year_by_title/{title}")
 def demo_2(title):
     return get_runtime_and_release_year(movie_title=title)
 
