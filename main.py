bb076fce5e41056b66193324c6b7feb291b18598
Juan Diego A.H <j.diego93@outlook.com>
2023-08-01 23:07:58 -0600

    demo 3 // collection data by title 1.1 (fixing end point errors)

--- main.py
+++ main.py
@@ -5,22 +5,16 @@ app = FastAPI()
 
 @app.get("/count_of_movies_by_language/{lang}")
 def demo_1(lang):
-    try:
-        count_movies_by_original_languages(language=lang)
-    except (ValueError):
-        return "Please provide only strings"
+    return count_movies_by_original_languages(language=lang)
+    
 
 
 @app.get("/get_runtime_and_release_year_by_title/{title0}")
 def demo_2(title0):
-    try:
-        get_runtime_and_release_year(movie_title=title0)
-    except (IndexError, ValueError):
-        return "No movie found"
+    return get_runtime_and_release_year(movie_title=title0)
+    
 
 @app.get("/get_collection_data_by_title/{title2}")
 def demo_3(title2):
-    try:
-        get_collection_information_by_title(title1=title2)
-    except (IndexError, ValueError):
-        return "No movie found"
+    return get_collection_information_by_title(title1=title2)
+    
